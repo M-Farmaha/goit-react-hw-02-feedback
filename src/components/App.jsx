7eb07e432ba698cf0a283c66b875c83a5a9544a9
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2023-05-06 03:22:31 +0300

    first task completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Section } from './Section';
+import { FeedbackOptions } from './FeedbackOptions';
+import { Statistics } from './Statistics';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClick = e => {
+    const value = e.currentTarget.textContent;
+    this.setState(prevState => {
+      return { [value]: prevState[value] + 1 };
+    });
+  };
+
+  countTotalFeedback = () => {
+    const values = Object.values(this.state);
+    let total = 0;
+    for (const value of values) {
+      total += value;
+    }
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const good = this.state.good;
+    const all = this.countTotalFeedback();
+    if (all === 0) {
+      return 0;
+    }
+    return ((good / all) * 100).toFixed(0);
+  };
+
+  render() {
+    const {
+      state,
+      handleClick,
+      countTotalFeedback,
+      countPositiveFeedbackPercentage,
+    } = this;
+
+    return (
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={state} onLeaveFeedback={handleClick} />
+        <Statistics
+          options={state}
+          total={countTotalFeedback}
+          positivePercentage={countPositiveFeedbackPercentage}
+        ></Statistics>
+      </Section>
+    );
+  }
+}
